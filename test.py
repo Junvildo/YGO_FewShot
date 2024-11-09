8fae9daa5867d09eeea67731d05c12fd4c42b12a
Junvildo <tienloc317@gmail.com>
2024-11-09 12:42:22 +0700

    yolo2card2art

--- test.py
+++ test.py
@@ -1,360 +1,88 @@
-# from mobileone import mobileone
-# from torchsummary import summary
-# from torch import nn
-
-# class EmbeddedFeatureWrapper(nn.Module):
-#     """
-#     Wraps a base model with embedding layer modifications.
-#     """
-#     def __init__(self,
-#                  feature,
-#                  input_dim,
-#                  output_dim):
-#         super(EmbeddedFeatureWrapper, self).__init__()
-
-#         self.feature = nn.Sequential(
-#             feature.stage0,
-#             feature.stage1,
-#             feature.stage2,
-#             feature.stage3,
-#             feature.stage4,
-#             feature.gap
-#         )
-#         self.standardize = nn.LayerNorm(input_dim, elementwise_affine=False)
-
-#         self.remap = None
-#         if input_dim != output_dim:
-#             self.remap = nn.Linear(input_dim, output_dim, bias=False)
-
-#     def forward(self, images):
-#         x = self.feature(images)
-#         x = x.view(x.size(0), -1)
-#         x = self.standardize(x)
-
-#         if self.remap:
-#             x = self.remap(x)
-
-#         x = nn.functional.normalize(x, dim=1)
-
-#         return x
-
-#     def __str__(self):
-#         return "{}_{}".format(self.feature.name, str(self.embed))
-
-
-# baseline = mobileone(variant="s2")
-# model = EmbeddedFeatureWrapper(feature=baseline, input_dim=2048, output_dim=2048)
-
-# model = mobileone(variant="s2")
-# summary(model, (3, 7, 7))
-
-# from torch.utils.data import DataLoader
-# from torch.utils.data.dataloader import default_collate
-# from torchvision import transforms
-# from sampler import ClassBalancedBatchSampler
-# from data import CustomDataset
-
-
-# train_transform = transforms.Compose([
-#     transforms.Grayscale(num_output_channels=3),
-#     transforms.Resize((224, 224)),
-#     transforms.ToTensor()
-# ])
-
-# train_dataset = CustomDataset(root="./main_dataset", train=True, transform=train_transform)
-
-# train_sampler = ClassBalancedBatchSampler(train_dataset.class_labels_list, 2, 2)
-# train_loader = DataLoader(train_dataset,
-#                             batch_sampler=train_sampler, num_workers=4,
-#                             pin_memory=True, drop_last=False, collate_fn=default_collate)
-
-# for data in train_loader:
-#     matrix, class_labels = data
-#     print(matrix.shape, class_labels.shape)
-#     break
-
-
-# train_loader = DataLoader(train_dataset,
-#                             num_workers=4, batch_size=2,
-#                             pin_memory=True, drop_last=False, collate_fn=default_collate)
-
-# for data in train_loader:
-#     matrix, class_labels = data
-#     print(matrix.shape, class_labels.shape)
-#     break
-
-# import faiss
-# import numpy as np
-# from models import EmbeddedFeatureWrapper
-# from torch.utils.data import DataLoader
-# from torchvision import transforms
-# from data import CustomDataset
-# from torch.utils.data.dataloader import default_collate
-# from extract_features import extract_feature
-# from mobileone import mobileone
-# import torch
-# import os
-# import time
-
-# os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-# from PIL import Image
-
-# def create_query_embedding(image_path, model, device):
-#     # Load and preprocess the image
-#     image = Image.open(image_path).convert('RGB')
-#     transform = transforms.Compose([
-#         transforms.Grayscale(num_output_channels=3),
-#         transforms.Resize((56, 56)),
-#         transforms.ToTensor(),
-#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-#     ])
-#     image_tensor = transform(image).unsqueeze(0).to(device)
-
-#     # Evaluate the model to get the embedding
-#     model.eval()
-#     with torch.no_grad():
-#         query_embedding = model(image_tensor)
-
-#     return query_embedding.cpu().numpy()
-
-
-# def retrieve_knn_faiss_euclidean(query_embeddings, db_embeddings, k):
-#     """
-#         Retrieve k nearest neighbors based on Euclidean distance using CPU.
-
-#         Args:
-#             query_embeddings:           numpy array of size [NUM_QUERY_IMAGES x EMBED_SIZE]
-#             db_embeddings:              numpy array of size [NUM_DB_IMAGES x EMBED_SIZE]
-#             k:                          number of nn results to retrieve excluding query
-
-#         Returns:
-#             dists:                      numpy array of size [NUM_QUERY_IMAGES x k], distances of k nearest neighbors
-#                                         for each query
-#             retrieved_db_indices:       numpy array of size [NUM_QUERY_IMAGES x k], indices of k nearest neighbors
-#                                         for each query
-#     """
-#     index = faiss.IndexFlatL2(db_embeddings.shape[1])  # Euclidean distance index
-#     index.add(db_embeddings)
-#     dists, retrieved_result_indices = index.search(query_embeddings, k + 1)
-
-#     return dists, retrieved_result_indices
-
-# if __name__ == "__main__":
-#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-#     model = EmbeddedFeatureWrapper(feature=mobileone(variant="s2"), input_dim=2048, output_dim=2048)
-#     state_dict = torch.load("./finetuned_models/s2_56_epoch_45.pth", map_location=device, weights_only=True)
-#     state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
-#     model.load_state_dict(state_dict)
-#     trans = transforms.Compose([
-#         transforms.Grayscale(num_output_channels=3),
-#         transforms.Resize((56, 56)),
-#         transforms.ToTensor(),
-#         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-#     ])
-#     dataset = CustomDataset("D:/Coding/ygo_fsl/dataset/", transform=trans, train=True)
-#     if not os.path.exists("embeddings.npy"):
-
-#         dataloader = DataLoader(dataset, batch_size=8, num_workers=4,pin_memory=True, collate_fn=default_collate)
-#         embeddings, labels = extract_feature(loader=dataloader, model=model, device=device)
-#         # Save embeddings to a file
-#         np.save("embeddings.npy", embeddings)
-
-#     embeddings = np.load("embeddings.npy")
-#     binary_db_embeddings = np.require(embeddings > 0, dtype='float32')
-
-
-#     # Example usage
-#     time_start = time.time()
-#     image_path = "detected_object_3.jpg"
-#     query_embedding = create_query_embedding(image_path, model, device)
-
-#     binary_query_embeddings = np.require(query_embedding > 0, dtype='float32')
-
-
-#     dists, retrieved_result_indices = retrieve_knn_faiss_euclidean(binary_query_embeddings, binary_db_embeddings, 1)
-#     print(dists)
-#     print(retrieved_result_indices)
-#     # Convert retrieved indices to image names or image classes
-#     image_names = [dataset.image_paths[i] for i in retrieved_result_indices[0]]
-#     image_classes = [dataset.class_labels[i] for i in retrieved_result_indices[0]]
-
-#     time_end = time.time()
-#     print("Time cost: {} seconds".format(time_end - time_start))
-
-#     print("Retrieved Image Names:", image_names)
-#     print("Retrieved Image Classes:", image_classes)
-
-
-
-# import json
-# import requests
-
-
-# with open('all_cards.json') as f:
-#     data = json.load(f)
-# data = requests.get('https://db.ygoprodeck.com/api/v7/cardinfo.php').json()
-
-# cards = [card for card in data['data'] if card['type'] not in ['Token', 'Skill Card']]
-
-# Card image_url list
-# url_list = [img['image_url_cropped'] for card in cards for img in card['card_images']]
-
-# card_url_dict = {}
-# for card in cards:
-#     if len(card['card_images']) != 1:
-#         card_url_dict[card['id']] = [url['image_url_cropped'] for url in card['card_images']]
-#     else:
-#         card_url_dict[card['id']] = [card['card_images'][0]['image_url_cropped']]
-
-
-
-# with open('card_url_dict.json', 'w') as f:
-#     json.dump(card_url_dict, f)
-
-from ultralytics import YOLO
 import cv2
-import faiss
 import numpy as np
-from models import EmbeddedFeatureWrapper
-from torch.utils.data import DataLoader
-from torchvision import transforms
-from data import CustomDataset
-from torch.utils.data.dataloader import default_collate
-from extract_features import extract_feature
-from mobileone import mobileone, reparameterize_model
-import torch
-import os
 import time
 
-os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
-from PIL import Image
+# Load the input image
+img = cv2.imread('yolo_detected/detected_object_8.jpg')
 
-def create_query_embedding(image_path, model, device):
-    # Load and preprocess the image
-    image = Image.open(image_path).convert('RGB')
-    transform = transforms.Compose([
-        transforms.Grayscale(num_output_channels=3),
-        transforms.Resize((56, 56)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-    image_tensor = transform(image).unsqueeze(0).to(device)
+# Preprocessing
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+eq_image = cv2.equalizeHist(gray)
+blur = cv2.medianBlur(eq_image, 3)
+_, thresh = cv2.threshold(blur, 100, 255, cv2.THRESH_BINARY)
 
-    # Evaluate the model to get the embedding
-    model.eval()
-    model = model.to(device)
-    model = reparameterize_model(model)
-    with torch.no_grad():
-        query_embedding = model(image_tensor)
+# Get contours
+contours, _ = cv2.findContours(thresh, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+imx, imy = img.shape[:2]
+lp_area = (imx * imy) / 10
 
-    return query_embedding.cpu().numpy()
+# Helper functions for perspective transform
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
 
+def four_point_transform(image, pts):
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype="float32")
+    M = cv2.getPerspectiveTransform(rect, dst)
+    return cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
-def retrieve_knn_faiss_euclidean(query_embeddings, db_embeddings, k):
-    """
-        Retrieve k nearest neighbors based on Euclidean distance using CPU.
-
-        Args:
-            query_embeddings:           numpy array of size [NUM_QUERY_IMAGES x EMBED_SIZE]
-            db_embeddings:              numpy array of size [NUM_DB_IMAGES x EMBED_SIZE]
-            k:                          number of nn results to retrieve excluding query
-
-        Returns:
-            dists:                      numpy array of size [NUM_QUERY_IMAGES x k], distances of k nearest neighbors
-                                        for each query
-            retrieved_db_indices:       numpy array of size [NUM_QUERY_IMAGES x k], indices of k nearest neighbors
-                                        for each query
-    """
-    index = faiss.IndexFlatL2(db_embeddings.shape[1])  # Euclidean distance index
-    index.add(db_embeddings)
-    dists, retrieved_result_indices = index.search(query_embeddings, k + 1)
-
-    return dists, retrieved_result_indices
-
-if __name__ == "__main__":
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model = EmbeddedFeatureWrapper(feature=mobileone(variant="s2"), input_dim=2048, output_dim=2048)
-    state_dict = torch.load("./finetuned_models/s2_56_epoch_45.pth", map_location=device, weights_only=True)
-    state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
-    model.load_state_dict(state_dict)
-    trans = transforms.Compose([
-        transforms.Grayscale(num_output_channels=3),
-        transforms.Resize((56, 56)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
-    dataset = CustomDataset("D:/Coding/ygo_fsl/dataset/", transform=trans, train=True)
-    if not os.path.exists("embeddings.npy"):
-
-        dataloader = DataLoader(dataset, batch_size=8, num_workers=4,pin_memory=True, collate_fn=default_collate)
-        embeddings, labels = extract_feature(loader=dataloader, model=model, device=device)
-        # Save embeddings to a file
-        np.save("embeddings.npy", embeddings)
-
-    embeddings = np.load("embeddings.npy")
-    binary_db_embeddings = np.require(embeddings > 0, dtype='float32')
-
-
-    # Example usage
+# Extract artwork function from card2art.py
+def extract_artwork(image, thresh_val=180, img_size=56):
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    eq_image = cv2.equalizeHist(gray_image)
+    _, binary_image = cv2.threshold(eq_image, thresh_val, 255, cv2.THRESH_BINARY)
+    kernel = np.ones((3, 3), np.uint8)
+    processed_image = cv2.erode(binary_image, kernel, iterations=3)
+    processed_image = cv2.dilate(processed_image, kernel, iterations=3)
+    contours, _ = cv2.findContours(processed_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
+    max_contour = max(contours, key=cv2.contourArea)
+    x, y, w, h = cv2.boundingRect(max_contour)
+    
+    if image.shape[0] > image.shape[1]:
+        art = image[int((h * 4) * 0.2):y, x:x + w] if (image.shape[0] - (y + h)) < y else image[y + h:int(((y + h) * 4) * 0.8), x:x + w]
+    else:
+        art = image[y:y + h, x + w:int((w * 4) * 0.8)] if x < (image.shape[1] // 2) else image[y:y + h, int((w * 4) * 0.2):x]
+    
+    return cv2.resize(art, (img_size, img_size))
 
-    ## Load YOLOv8 model
-    # yolo_model = YOLO('./finetuned_models/yolo_ygo.pt')
+# Detect rectangles, correct perspective, and extract artwork
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+    if len(approx) >= 4 and cv2.contourArea(cnt) > lp_area:
+        p1, p2, p3, p4 = (0, 0), (img.shape[1], 0), (0, img.shape[0]), (img.shape[1], img.shape[0])
+        closest_points = [None] * 4
+        distances = [float('inf')] * 4
+        for p in cnt.reshape(-1, 2):
+            for i, corner in enumerate([p1, p2, p3, p4]):
+                d = np.linalg.norm(p - corner)
+                if d < distances[i]:
+                    distances[i] = d
+                    closest_points[i] = p
+        pts = np.array(closest_points, dtype="float32")
+        warped = four_point_transform(img.copy(), pts)
 
-    ## Load and prepare image
-    # img_path = './real_life_images/ygo_field2.jpg'
-    # img = cv2.imread(img_path)
+        # Extract artwork from warped perspective
+        art = extract_artwork(warped)
+        cv2.imshow("Extracted Artwork", art)
 
-    ## Run inference
-    # results = yolo_model(img)
-
-    ## Extract bounding box coordinates and confidence scores
-    # detected_objects = []
-    # for i, (box, conf) in enumerate(zip(results[0].boxes.xyxy, results[0].boxes.conf)):
-    #     if conf >= 0.5:  # Filter by confidence (greater than 50%)
-    #         x1, y1, x2, y2 = map(int, box)  # Extract bounding box coordinates
-    #         # Crop the detected object from the image
-    #         cropped_img = img[y1:y2, x1:x2]
-    #         detected_objects.append(cropped_img)
-
-    # # Create a batch of detected objects
-    # batch_size = 8
-    # detected_objects_batches = [detected_objects[i:i+batch_size] for i in range(0, len(detected_objects), batch_size)]
-
-    # # Extract embeddings for each batch of detected objects
-    # all_embeddings = []
-    # for detected_objects_batch in detected_objects_batches:
-    #     images = [Image.fromarray(obj) for obj in detected_objects_batch]
-    #     image_tensors = [trans(img).unsqueeze(0).to(device) for img in images]
-    #     image_tensors = torch.cat(image_tensors, dim=0)
-    #     with torch.no_grad():
-    #         embeddings_batch = model(image_tensors)
-    #     all_embeddings.extend(embeddings_batch.cpu().numpy())
-
-    # # # Convert embeddings to binary format
-    # # # binary_query_embeddings = np.require(all_embeddings > 0, dtype='float32')
-    # # binary_query_embeddings = np.stack([np.require(embedding > 0, dtype='float32') for embedding in all_embeddings], axis=0)
-
-    # # # Retrieve k nearest neighbors for each detected object
-    # # dists, retrieved_result_indices = retrieve_knn_faiss_euclidean(binary_query_embeddings, binary_db_embeddings, 1)
-
-    # # # Convert retrieved indices to image names or image classes
-    # # image_names = []
-    # # image_classes = []
-    # # for i in range(len(retrieved_result_indices)):
-    # #     image_name = dataset.image_paths[retrieved_result_indices[i][0]]
-    # #     image_names.append(image_name)
-    # #     image_class = dataset.class_labels[retrieved_result_indices[i][0]]
-    # #     image_classes.append(image_class)
-
-    # # Display each detected object with its retrieved image
-    # for i, obj in enumerate(detected_objects):  # zip(detected_objects, image_names, image_classes)):
-    #     # window_name = f"Detected Object {i + 1}"
-    #     # cv2.imshow(window_name, obj)
-    #     cv2.imwrite(f"detected_object_{i + 1}.jpg", obj)
-    #     # print(f"Detected Object {i + 1}: {name} with class {class_}")
-
-
-    # # Press any key to close all windows
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- yolo2card.py
+++ yolo2card.py
@@ -1,74 +1,140 @@
+"""
+Task: Detect card corners and fix perspective
+"""
+
+
 import cv2
 import numpy as np
 
-def auto_detect_card_corners_and_transform(image_path):
-    # Load the image
-    image = cv2.imread(image_path)
-    if image is None:
-        print("Error: Image not found.")
-        return None
 
-    # Resize for easier processing if needed
-    scale_percent = 40  # adjust scale as needed
-    width = int(image.shape[1] * scale_percent / 100)
-    height = int(image.shape[0] * scale_percent / 100)
-    image = cv2.resize(image, (width, height))
+img = cv2.imread('yolo_detected/detected_object_1.jpg')
 
-    # Convert to grayscale
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Apply Gaussian blur
-    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-    # Edge detection
-    edges = cv2.Canny(blurred, 200, 1000)
 
-    # Find contours
-    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+eq_image = cv2.equalizeHist(gray)
+blur = cv2.medianBlur(eq_image, 3)
+_, thresh = cv2.threshold(blur, 100 , 255, cv2.THRESH_BINARY)
 
-    # Sort contours by area and pick the largest one (assumed to be the card)
-    contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    card_contour = contours[0]
 
-    # Approximate the contour to a quadrilateral
-    epsilon = 0.02 * cv2.arcLength(card_contour, True)
-    approx = cv2.approxPolyDP(card_contour, epsilon, True)
+## Get contours
+contours,h = cv2.findContours(thresh,cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+print(len(contours))
 
-    if len(approx) != 4:
-        print("Error: Could not detect 4 corners of the card.")
-        return None
 
-    # Obtain the four points
-    card_corners = np.array([point[0] for point in approx], dtype="float32")
+## only draw contour that have big areas
+imx = img.shape[0]
+imy = img.shape[1]
+lp_area = (imx * imy) / 10
 
-    # Order the corners in a consistent way: top-left, top-right, bottom-right, bottom-left
-    s = card_corners.sum(axis=1)
-    diff = np.diff(card_corners, axis=1)
-    top_left = card_corners[np.argmin(s)]
-    bottom_right = card_corners[np.argmax(s)]
-    top_right = card_corners[np.argmin(diff)]
-    bottom_left = card_corners[np.argmax(diff)]
-    ordered_corners = np.array([top_left, top_right, bottom_right, bottom_left], dtype="float32")
 
-    # Define the dimensions for the bird's eye view
-    width, height = 300, 200
-    dst_corners = np.array([
+
+#################################################################
+# Four point perspective transform
+# https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+#################################################################
+
+def order_points(pts):
+    # initialzie a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype = "float32")
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis = 1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis = 1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    # return the ordered coordinates
+    return rect
+
+
+def four_point_transform(image, pts):
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
         [0, 0],
-        [width, 0],
-        [width, height],
-        [0, height]
-    ], dtype="float32")
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    # return the warped image
+    return warped
 
-    # Compute the perspective transform matrix and apply it
-    matrix = cv2.getPerspectiveTransform(ordered_corners, dst_corners)
-    bird_eye_view = cv2.warpPerspective(image, matrix, (width, height))
 
-    # Display the result
-    cv2.imshow("Original Image", image)
-    cv2.imshow("Bird's Eye View", bird_eye_view)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+#################################################################
 
-    return bird_eye_view
 
-# Usage
-image_path = 'yolo_detected/detected_object_5.jpg'
-bird_eye_view = auto_detect_card_corners_and_transform(image_path)
+## Get only rectangles given exceeding area
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt,0.01 * cv2.arcLength(cnt, True), True)
+
+
+    if len(approx) >= 4 and cv2.contourArea(cnt) > lp_area:
+        print("rectangle")
+
+        # find points along contours, the points are, closest to the top left, top right, bottom left, bottom right of the image
+        p1 = (0, 0)
+        p2 = (img.shape[1], 0)
+        p3 = (0, img.shape[0])
+        p4 = (img.shape[1], img.shape[0])
+        dist1 = float('inf')
+        dist2 = float('inf')
+        dist3 = float('inf')
+        dist4 = float('inf')
+        p1_contour = None
+        p2_contour = None
+        p3_contour = None
+        p4_contour = None
+        for p in cnt.reshape(-1, 2):
+            d1 = np.linalg.norm(p - p1)
+            if d1 < dist1:
+                dist1 = d1
+                p1_contour = p
+            d2 = np.linalg.norm(p - p2)
+            if d2 < dist2:
+                dist2 = d2
+                p2_contour = p
+            d3 = np.linalg.norm(p - p3)
+            if d3 < dist3:
+                dist3 = d3
+                p3_contour = p
+            d4 = np.linalg.norm(p - p4)
+            if d4 < dist4:
+                dist4 = d4
+                p4_contour = p
+
+        # Perform a perspective transform using the points closest to the image corners
+        tmp_img = img.copy()
+        pts = np.array([p1_contour, p2_contour, p3_contour, p4_contour], dtype="float32")
+        warped = four_point_transform(tmp_img, pts)
+        cv2.imshow("Warped Perspective", warped)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
